436c25bae174d379509e109e6644bbb2293c9611
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2023-09-29 14:50:37 +0800

    Update base_fun.sh

--- file/base_fun.sh
+++ file/base_fun.sh
@@ -10,9 +10,7 @@ function random_str {
 }
 # 打印文字颜色方法
 echoTxtColor(){
-	
 	colorV="1"
-	
 	if [[ $2 = 'red' ]];
 	then
 		colorV="1"
@@ -23,6 +21,13 @@ echoTxtColor(){
 	then
 		colorV="3"
 	fi
-	
 	echo -e "\033[3${colorV}m ${1} \033[0m"
 }
+# 获取github 项目中的最新版本号
+getVersion(){
+	# 获取github项目中最新版本号
+	echo $(wget -qO- -t1 -T2 "https://api.github.com/${1}/m3u8-downloader/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+}
+# 调用示例
+# 传入项目名称  "repos/llychao"
+# getVersion "repos/llychao"
