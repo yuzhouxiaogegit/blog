ef69dcba3c43e0824819d8ded3b5c2c70685ab9c
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2025-05-31 09:30:04 +0800

    base_fun.sh

--- file/base_fun.sh
+++ file/base_fun.sh
@@ -2,7 +2,7 @@
 
 #base64加密方法
 #echo $(base64_en_fun '12')
-base64_en_fun(){
+yzxg_base64_en(){
 	local loopNum=${2:-1} # 加密次数
 	local tempStr=${1:-} # 加密字符串
 	for (( i=1; i<=$loopNum; i++ ))
@@ -10,12 +10,11 @@ base64_en_fun(){
 	    tempStr=$(echo -n "$tempStr" | base64)
 	done
 	echo $tempStr
-
 }
 
 #base64解密方法
 #echo $(base64_de_fun "$(echo $(base64_en_fun '12'))")
-base64_de_fun(){
+yzxg_base64_de(){
 	local loopNum=${2:-1} # 解密次数
 	local tempStr=${1:-} # 解密字符串
 	for (( i=1; i<=$loopNum; i++ ))
@@ -27,7 +26,7 @@ base64_de_fun(){
 
 #字符加密方法
 #echo $(char_en_fun 'qqqqq' 5)
-char_en_fun(){
+yzxg_char_en(){
 	local tempStr=${1:-} #加密值
 	local salt=${2:-0} #加密盐
 	for (( i=0; i<${#tempStr}; i++ )); do
@@ -36,12 +35,11 @@ char_en_fun(){
             local new_ascii=$((original_ascii + salt))
                   printf "%d " "$new_ascii"
 	done
-
 }
 
 #字符串解密方法
 #echo $(char_de_fun "$(char_en_fun 'qqqqq' 5)" 5)
-char_de_fun(){
+yzxg_char_de(){
 	local tempStr=${1:-} # 解密值
 	local salt=${2:-0} # 解密盐
 	local decoded_string=''
@@ -55,19 +53,19 @@ char_de_fun(){
 
 #指定区间随机数
 #echo $(random_num_fun 8 16)
-function random_num_fun {
+yzxg_random_num {
    shuf -i $1-$2 -n1
 }
 
 #指定区间随机字符串
 #echo $(random_str_fun 8 19)
-function random_str_fun {
+yzxg_random_str {
    echo $((cat /proc/sys/kernel/random/uuid || uuidgen) | cut -c $1-$2) | sed 's/-//g'
 }
 
 # 打印文字颜色方法
 #echo_txt_color_fun "您的文字颜色打印成功" "green"
-echo_txt_color_fun(){
+yzxg_echo_txt_color(){
 	colorV="1"
 	if [[ $2 = 'red' ]];
 	then
@@ -86,7 +84,7 @@ echo_txt_color_fun(){
 # $1 = 软件releases地址  参数示列值（https://www.ffmpeg.org/releases/）
 # $2 = 软件名称：ffmpeg  参数示列值（ffmpeg-7.0.2.tar.xz）  
 # 函数调用示例 get_new_version_num_fun 'https://www.ffmpeg.org/releases/' 'ffmpeg'
-get_new_version_num_fun(){
+yzxg_get_new_version_num(){
 	if [[ $1 =~ github.com ]]
 		then
 			wget --timeout=10 -qO- $1 | grep -Po '(?<=/tag/)[vV]?([0-9]+\.)+[0-9]+' | head -n 1
@@ -96,8 +94,7 @@ get_new_version_num_fun(){
 }
 
 # 获取系统判断包管理器
-get_package_manage_fun(){
-
+yzxg_get_package_manage(){
 	if command -v yum &> /dev/null; then
 	    # 系统使用 yum (可能是 CentOS/RHEL/Fedora)
 	    echo "yum"
@@ -117,5 +114,4 @@ get_package_manage_fun(){
 	    # 无法确定包管理器类型
 	    echo "unknown"
 	fi
-
 }
