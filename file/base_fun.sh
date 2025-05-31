4a0a6c80ed9aaaa7e40e09964a3d62a2bef8ece2
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2025-05-31 13:28:46 +0800

    base_fun.sh

--- file/base_fun.sh
+++ file/base_fun.sh
@@ -1,72 +1,72 @@
 #!/usr/bin/env bash
 
 #base64加密方法
-#echo $(base64_en_fun '12')
+#echo $(yzxg_base64_en '12')
 yzxg_base64_en(){
 	local loopNum=${2:-1} # 加密次数
 	local tempStr=${1:-} # 加密字符串
 	for (( i=1; i<=$loopNum; i++ ))
 	do
-	    tempStr=$(echo -n "$tempStr" | base64)
+	    tempStr=$(printf '%s' "$tempStr" | base64)
 	done
-	echo $tempStr
+	echo $(echo $tempStr | grep -Eo '[A-Za-z0-9+/=]+')
 }
 
 #base64解密方法
-#echo $(base64_de_fun "$(echo $(base64_en_fun '12'))")
+#echo $(yzxg_base64_de "$(echo $(yzxg_base64_en '12'))")
 yzxg_base64_de(){
 	local loopNum=${2:-1} # 解密次数
 	local tempStr=${1:-} # 解密字符串
 	for (( i=1; i<=$loopNum; i++ ))
 	do
-	    tempStr=$(echo -n "$tempStr" | base64 -d)
+	    tempStr=$(printf '%s' "$tempStr" | grep -Eo '[A-Za-z0-9+/=]+' | base64 -d)
 	done
 	echo $tempStr
 }
 
-#字符加密方法
-#echo $(char_en_fun 'qqqqq' 5)
+#字符加密方法，支持键盘字符
+#echo $(yzxg_char_en 'qqqqq' 5)
 yzxg_char_en(){
 	local tempStr=${1:-} #加密值
 	local salt=${2:-0} #加密盐
 	for (( i=0; i<${#tempStr}; i++ )); do
 	    local char="${tempStr:$i:1}"
-	    local original_ascii=$(printf "%d" "'$char")
-            local new_ascii=$((original_ascii + salt))
-                  printf "%d " "$new_ascii"
+	    local originalAscii=$(printf "%d" "'$char")
+         local newAscii=$((originalAscii + salt))
+                  printf "%d " "$newAscii"    
 	done
 }
 
-#字符串解密方法
-#echo $(char_de_fun "$(char_en_fun 'qqqqq' 5)" 5)
+#字符串解密方法，支持键盘字符
+#echo $(yzxg_char_de "$(yzxg_char_en 'qqqqq' 5)" 5)
 yzxg_char_de(){
 	local tempStr=${1:-} # 解密值
 	local salt=${2:-0} # 解密盐
-	local decoded_string=''
+	local decodedString=''
      for code in $tempStr; do
-         local original_ascii=$((code - salt))
-         local char_hex=$(printf "%x" "$original_ascii") 
-               decoded_string+=$(printf "\\x$char_hex")
+         local originalAscii=$((code - salt))
+         local charHex=$(printf "%x" "$originalAscii") 
+               decodedString+=$(printf "\\x$charHex")
      done
-     echo "$decoded_string"
+     echo $decodedString
 }
 
 #指定区间随机数
-#echo $(random_num_fun 8 16)
+#echo $(yzxg_random_num 8 16)
 yzxg_random_num(){
    shuf -i $1-$2 -n1
 }
 
 #指定区间随机字符串
-#echo $(random_str_fun 8 19)
+#echo $(yzxg_random_str 8 19)
 yzxg_random_str(){
    echo $((cat /proc/sys/kernel/random/uuid || uuidgen) | cut -c $1-$2) | sed 's/-//g'
 }
 
 # 打印文字颜色方法
-#echo_txt_color_fun "您的文字颜色打印成功" "green"
+# yzxg_echo_txt_color "您的文字颜色打印成功" "green"
 yzxg_echo_txt_color(){
-	colorV="1"
+	local colorV="1"
 	if [[ $2 = 'red' ]];
 	then
 		colorV="1"
@@ -83,7 +83,7 @@ yzxg_echo_txt_color(){
 # 获取最新软件版本号码,非gitbub版本号也能获取,例如:ffmpeg等
 # $1 = 软件releases地址  参数示列值（https://www.ffmpeg.org/releases/）
 # $2 = 软件名称：ffmpeg  参数示列值（ffmpeg-7.0.2.tar.xz）  
-# 函数调用示例 get_new_version_num_fun 'https://www.ffmpeg.org/releases/' 'ffmpeg'
+# 函数调用示例 yzxg_get_new_version_num 'https://www.ffmpeg.org/releases/' 'ffmpeg'
 yzxg_get_new_version_num(){
 	if [[ $1 =~ github.com ]]
 		then
