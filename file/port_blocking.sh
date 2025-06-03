947f81871536dbfcd21bf7529df9625bf7c6e5f6
宇宙小哥 <52238373+yuzhouxiaogegit@users.noreply.github.com>
2025-06-03 13:29:11 +0800

    port_blocking.sh

--- file/port_blocking.sh
+++ file/port_blocking.sh
@@ -1,65 +1,59 @@
 #!/bin/bash
 
-# 打印文字颜色方法
-echoTxtColor(){
-	
-	colorV="1"
-	
-	if [[ $2 = 'red' ]];
-	then
-		colorV="1"
-	elif [[ $2 = 'green' ]];
-	then
-		colorV="2"
-	elif [[ $2 = 'yellow' ]];
-	then
-		colorV="3"
+# 脚本函数初始化
+source <(timeout 5 curl -sL https://raw.githubusercontent.com/yuzhouxiaogegit/blog/main/file/base_fun.sh) && clear
+
+read -p "请输入允许的ip通过防火墙【多个ip用空格间隔】(默认不处理):" ipList
+
+read -p "开放特定端口【多个端口用空格间隔】(默认不处理):" portList
+
+read -p "是否同时开放udp协议(默认n/y):" udpStatus
+
+# 支持 centos yum 命令 
+if [[ $(yzxg_get_package_manage) = 'yum' ]]
+then
+
+	if ! command -v firewall-cmd &>/dev/null; then
+	   yum install -y firewalld && systemctl restart firewalld.service
 	fi
 	
-	echo -e "\033[3${colorV}m ${1} \033[0m"
-}
-# 调用示例
-# echoTxtColor "您的文字颜色打印成功" "green"
-
-yum install -y firewalld # 安装防火墙
-
-systemctl restart firewalld.service # 重启防火墙
-
-read -p "请输入允许的ip通过防火墙【多个ip用英文逗号间隔】,输入n取消:" ipList
-
-resIpList=(${ipList//,/ })
-
-for ip in ${resIpList[@]}; do
-	if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-		firewall-cmd --zone=public --add-rich-rule 'rule family="ipv4" source address="'${ip}'" accept' --permanent
+	# 允许特定ip通过防火墙 start -->
+	ipList=$(echo $ipList | grep -Po '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})+')
+	if [[ $ipList && $ipList =~ ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)+ ]]; then
+		for reIp in $(firewall-cmd --list-rich-rules | grep -Po '([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})+'); do
+			firewall-cmd --zone=public --remove-rich-rule 'rule family="ipv4" source address="'$reIp'" accept' --permanent
+		done
+		for acIp in $ipList; do
+			firewall-cmd --zone=public --add-rich-rule 'rule family="ipv4" source address="'${acIp}'" accept' --permanent
+		done
 	fi
-done
+	# 允许特定ip通过防火墙 end <--
 
-read -p "是否屏蔽非80,443的端口:(默认y/n)" portClose
-
-if [[ $portClose == 'y' || $portClose == '' ]]; then
-
-	portList=$(firewall-cmd --list-ports)
-	resPortList=(${portList// / })
- 
-	for v in ${resPortList[@]}; do
-		if [[ $v == '80/tcp' || $v == '80/udp' || $v == '443/tcp' || $v == '443/udp' ]]; then
-			continue
-		fi
-		firewall-cmd --zone=public --remove-port=$v --permanent
-	done
+	# 开放特定端口 start -->
+	if [[ $portList ]]; then
+		for oldPort in $(firewall-cmd --list-ports | grep -Po '[^\s]+'); do
+			firewall-cmd --zone=public --remove-port=$oldPort --permanent
+		done
+		for port in $(echo $portList | grep -Po '\d+'); do
+			if (( port >= 1 && port <= 65535 )); then
+				firewall-cmd --zone=public --add-port=$port/tcp --permanent
+				if [[ $udpStatus == 'y' ]]; then
+					firewall-cmd --zone=public --add-port=$port/udp --permanent
+				fi
+			fi
+		done
+	fi
+	# 开放特定端口 end <--
+	firewall-cmd --reload
+	systemctl restart firewalld.service
+else
+	yzxg_echo_txt_color "暂不支持您的防火墙，等待更新" "green"
 fi
 
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=80/udp --permanent
-firewall-cmd --zone=public --add-port=443/tcp --permanent
-firewall-cmd --zone=public --add-port=443/udp --permanent
-firewall-cmd --reload
-
 echo -e "\n"
-echoTxtColor "防火墙开放端口列表" "green"
+yzxg_echo_txt_color "防火墙开放端口列表" "green"
 firewall-cmd --list-ports
 
 echo -e "\n"
-echoTxtColor "允许通过防火墙的ip" "green"
+yzxg_echo_txt_color "查看允许通过防火墙的ip列表" "green"
 firewall-cmd --list-rich-rules
